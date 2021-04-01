063d48c0bec6748dfce870f29ca71d1e024b0f8a
Reinier de Leon <reinier@RMac.local>
2021-04-01 19:09:24 +1100

    folders made

--- SourceCode_and_Documentation/backend/main.py
+++ SourceCode_and_Documentation/backend/main.py
@@ -1,6 +1,13 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 @app.get("/")
 async def root():
@@ -9,3 +16,9 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"recieved item_id": item_id}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "pr": item.price}
+
+@app.
