d50962e17269a597992ab76618e32ef46d149c07
AD9000 <damle.atharv16@gmail.com>
2021-04-07 15:47:12 +1000

    dirty dashboard

--- SourceCode_and_Documentation/frontend/src/App.tsx
+++ SourceCode_and_Documentation/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { MapWrapper } from "./components/Map";
 import "./App.css";
+import { Home } from "./pages/Home";
 
 const App = () => {
   return (
-    <div style={{ height: "100%" }}>
-      <MapWrapper />
-    </div>
+    // <div style={{ height: "100%" }}>
+    //   <MapWrapper />
+    // </div>
+    <Home />
   );
 };
 
---
+++ SourceCode_and_Documentation/frontend/src/components/Tag.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tag = () => {
+  return null;
+};
+
+export { Tag };
