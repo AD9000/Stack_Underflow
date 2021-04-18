a6fdd8175e1f1d134bbec90cb64670935e53e41d
Jiaqi <blacker0lily311@gmail.com>
2021-04-19 00:55:43 +1000

    added class Comments, fixed DateTime in Tags and Comments

--- SourceCode_and_Documentation/backend/models.py
+++ SourceCode_and_Documentation/backend/models.py
@@ -1,8 +1,8 @@
 import sqlalchemy
-from sqlalchemy import Boolean, Column, ForeignKey, Numeric, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Numeric, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
 from database import Base
-from datetime import datetime
+#from datetime import datetime
 
 class Users(Base):
     __tablename__ = "users"
@@ -16,7 +16,7 @@ class Users(Base):
 
 class Tags(Base):
     __tablename__ = "tags"
-    id = Column(String, primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     title = Column(String, nullable=False)
     region = Column(String, nullable=False)
     location = Column(String, nullable=False)
@@ -24,15 +24,21 @@ class Tags(Base):
     n_likes = Column(Integer, nullable=False, default=0)
     song = Column(String, nullable=False)
     caption = Column(String, nullable=False)
-    time_made = Column(String, nullable=False)
-    time_edited = Column(String, nullable=False)
+    time_posted = Column(DateTime(timezone=True), server_default=func.now())
+    time_edited = Column(DateTime(timezone=True), onupdate=func.now())
     username = Column(String, ForeignKey('users.username'))
+    comments = relationship("Comments")
 
-class Song(Base):
+class Songs(Base):
     __tablename__ = "songs"
-
     link = Column(String, primary_key = True, nullable=False)
     title = Column(String, nullable=False)
     artist = Column(String, nullable=False)
 
+class Comments(Base):
+    __tablename__ = "comments"
+    tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True, nullable=False)
+    author = Column(String, ForeignKey('users.username'), nullable=False)
+    content = Column(String, nullable=False)
+    time_posted = Column(DateTime(timezone=True), server_default=func.now())
 
