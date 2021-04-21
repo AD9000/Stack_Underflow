6bc01a4263a4dd033d5163a3b43fbe1bec03cc5e
Dimitri <z5259123@ad.unsw.edu.au>
2021-04-22 00:59:15 +1000

    Rego page ready

--- SourceCode_and_Documentation/frontend/src/pages/RegisterSuccess.tsx
+++ SourceCode_and_Documentation/frontend/src/pages/RegisterSuccess.tsx
@@ -1,18 +1,18 @@
 // import React from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
-import { handleRedirect } from '../components/Spotify-Api/spotifyApi';
+import { handleRedirect } from "../components/Spotify-Api/spotifyApi";
 import { Container, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   background: {
     height: "100%",
-    backgroundColor: "#051c3f"
+    backgroundColor: "#051c3f",
   },
   page: {
-    textAlign: 'center',
-    padding: '2rem 0',
-    color: 'white'
+    textAlign: "center",
+    padding: "2rem 0",
+    color: "white",
   },
   btn: {
     textTransform: "none",
@@ -20,7 +20,7 @@ const useStyles = makeStyles({
     fontSize: "x-large",
     margin: "0.5rem",
   },
-})
+});
 
 // Change name
 export default function RegisterSuccess() {
@@ -28,8 +28,8 @@ export default function RegisterSuccess() {
   let history = useHistory();
 
   const handleButton = () => {
-    history.push('/home');
-  }
+    history.push("/home");
+  };
 
   // Removes code from url, and uses it to fetch access/refresh tokens
   handleRedirect();
@@ -39,12 +39,13 @@ export default function RegisterSuccess() {
       <Container className={classes.page}>
         <h1>Congratulations! Your account has been created.</h1>
         <p>Click the button below to return to WorldPlay.</p>
-        <Button 
+        <Button
           variant="contained"
           color="primary"
           onClick={handleButton}
-          className={classes.btn}>
-            <b>Go</b>
+          className={classes.btn}
+        >
+          <b>Go</b>
         </Button>
       </Container>
     </div>
