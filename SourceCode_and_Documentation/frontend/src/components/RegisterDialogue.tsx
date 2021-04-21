9d92368fe435b706331679b526939b6f148fab5e
AD9000 <damle.atharv16@gmail.com>
2021-04-22 00:50:14 +1000

    fix register form re-render bug

--- SourceCode_and_Documentation/frontend/src/components/RegisterDialogue.tsx
+++ SourceCode_and_Documentation/frontend/src/components/RegisterDialogue.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -84,6 +84,7 @@ export default function RegisterDialog() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [passwordConfirm, setPasswordConfirm] = React.useState("");
+  const [stepDialog, setStepDialog] = useState<React.ReactElement | null>(null);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -141,140 +142,136 @@ export default function RegisterDialog() {
     }
   };
 
-  function Step1() {
-    return (
-      <div className={styles.dialog}>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          className={styles.closeBtn}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogTitle id="form-dialog-title">
-          <Typography className={styles.dialogTitle}>
-            <b>Register New Account</b>
-          </Typography>
-        </DialogTitle>
-        <DialogContent className={styles.dialogContent}>
-          <div className={styles.grid}>
-            <h3 style={{ marginRight: "1rem" }} className={styles.text}>
-              Username
-            </h3>
-            <TextField
-              // autoFocus
-              variant="outlined"
-              margin="dense"
-              id="username"
-              InputProps={{ className: styles.input }}
-              // placeholder="username"
-              onBlur={(e) => setUsername(e.target.value)}
-            />
-            <h3 style={{ marginRight: "1rem" }} className={styles.text}>
-              Email
-            </h3>
-            <TextField
-              // autoFocus
-              variant="outlined"
-              margin="dense"
-              id="email"
-              InputProps={{ className: styles.input }}
-              // placeholder="email"
-              onBlur={(e) => setEmail(e.target.value)}
-            />
-            <h3 style={{ marginRight: "1rem" }} className={styles.text}>
-              Password
-            </h3>
-            <TextField
-              // autoFocus
-              variant="outlined"
-              margin="dense"
-              id="password"
-              InputProps={{ className: styles.input }}
-              // placeholder={password}
-              onBlur={(e) => setPassword(e.target.value)}
-              type="password"
-            />
-            <h3 style={{ marginRight: "1rem" }} className={styles.text}>
-              Confirm Password
-            </h3>
-            <TextField
-              // autoFocus
-              variant="outlined"
-              margin="dense"
-              id="confirmPassword"
-              InputProps={{ className: styles.input }}
-              // placeholder="confirm password"
-              onBlur={(e) => setPasswordConfirm(e.target.value)}
-              type="password"
-            />
-          </div>
-          <p className={styles.text} style={{ textAlign: "center" }}>
-            Already have an account?{" "}
-            <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
-              Sign In
-            </a>
-          </p>
-        </DialogContent>
-        <DialogActions>
-          <p className={styles.text}>
-            <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
-              Help
-            </a>
-          </p>
-          <Button
-            variant="contained"
-            color="primary"
-            className={styles.btn}
-            onClick={handleNext}
-          >
-            <b style={{ fontSize: "large" }}>Next</b>
-          </Button>
-
-          {/* <Button onClick={handleClose} color="primary">
-          Subscribe
-          </Button> */}
-        </DialogActions>
-      </div>
-    );
-  }
-
-  function Step2() {
-    return (
-      <div className={styles.dialog}>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          className={styles.closeBtn}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogTitle id="form-dialog-title">
-          <Typography className={styles.dialogTitle}>
-            <b>Link Your Spotify Account</b>
-          </Typography>
-        </DialogTitle>
-        <DialogContent className={styles.dialogContent}>
-          <Button variant="contained" color="primary" className={styles.btn}>
-            <SpotifyLogin />
-          </Button>
-        </DialogContent>
-        <DialogActions>
-          <p className={styles.text}>
-            <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
-              Help
-            </a>
-          </p>
-        </DialogActions>
-      </div>
-    );
-  }
-
   const styles = useStyles();
 
-  let stepDialog;
-  if (step === 1) stepDialog = <Step1 />;
-  else stepDialog = <Step2 />;
+  useEffect(() => {
+    if (step === 1) {
+      setStepDialog(
+        <div className={styles.dialog}>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            className={styles.closeBtn}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogTitle id="form-dialog-title">
+            <Typography className={styles.dialogTitle}>
+              <b>Register New Account</b>
+            </Typography>
+          </DialogTitle>
+          <DialogContent className={styles.dialogContent}>
+            <div className={styles.grid}>
+              <h3 style={{ marginRight: "1rem" }} className={styles.text}>
+                Username
+              </h3>
+              <TextField
+                // autoFocus
+                variant="outlined"
+                margin="dense"
+                id="username"
+                InputProps={{ className: styles.input }}
+                // placeholder="username"
+                onBlur={(e) => setUsername(e.target.value)}
+              />
+              <h3 style={{ marginRight: "1rem" }} className={styles.text}>
+                Email
+              </h3>
+              <TextField
+                // autoFocus
+                variant="outlined"
+                margin="dense"
+                id="email"
+                InputProps={{ className: styles.input }}
+                // placeholder="email"
+                onBlur={(e) => setEmail(e.target.value)}
+              />
+              <h3 style={{ marginRight: "1rem" }} className={styles.text}>
+                Password
+              </h3>
+              <TextField
+                // autoFocus
+                variant="outlined"
+                margin="dense"
+                id="password"
+                InputProps={{ className: styles.input }}
+                // placeholder={password}
+                onBlur={(e) => setPassword(e.target.value)}
+                type="password"
+              />
+              <h3 style={{ marginRight: "1rem" }} className={styles.text}>
+                Confirm Password
+              </h3>
+              <TextField
+                // autoFocus
+                variant="outlined"
+                margin="dense"
+                id="confirmPassword"
+                InputProps={{ className: styles.input }}
+                // placeholder="confirm password"
+                onBlur={(e) => setPasswordConfirm(e.target.value)}
+                type="password"
+              />
+            </div>
+            <p className={styles.text} style={{ textAlign: "center" }}>
+              Already have an account?{" "}
+              <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
+                Sign In
+              </a>
+            </p>
+          </DialogContent>
+          <DialogActions>
+            <p className={styles.text}>
+              <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
+                Help
+              </a>
+            </p>
+            <Button
+              variant="contained"
+              color="primary"
+              className={styles.btn}
+              onClick={handleNext}
+            >
+              <b style={{ fontSize: "large" }}>Next</b>
+            </Button>
+
+            {/* <Button onClick={handleClose} color="primary">
+          Subscribe
+          </Button> */}
+          </DialogActions>
+        </div>
+      );
+    } else {
+      setStepDialog(
+        <div className={styles.dialog}>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            className={styles.closeBtn}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogTitle id="form-dialog-title">
+            <Typography className={styles.dialogTitle}>
+              <b>Link Your Spotify Account</b>
+            </Typography>
+          </DialogTitle>
+          <DialogContent className={styles.dialogContent}>
+            <Button variant="contained" color="primary" className={styles.btn}>
+              <SpotifyLogin />
+            </Button>
+          </DialogContent>
+          <DialogActions>
+            <p className={styles.text}>
+              <a href="http://localhost:3000" style={{ color: "#3481e1" }}>
+                Help
+              </a>
+            </p>
+          </DialogActions>
+        </div>
+      );
+    }
+  }, [step]);
 
   return (
     <div>
