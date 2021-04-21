f3fc55aaa43e10b0aba840a11212f29b3c8caa63
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-04-21 15:10:53 +1000

    Success page done

--- SourceCode_and_Documentation/frontend/src/pages/RegisterSuccess.tsx
+++ SourceCode_and_Documentation/frontend/src/pages/RegisterSuccess.tsx
@@ -1,10 +1,30 @@
 // import React from "react";
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from "@material-ui/styles";
 import { handleRedirect } from '../components/Spotify-Api/spotifyApi';
+import { Container, Button } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  background: {
+    height: "100%",
+    backgroundColor: "#051c3f"
+  },
+  page: {
+    textAlign: 'center',
+    padding: '2rem 0',
+    color: 'white'
+  },
+  btn: {
+    textTransform: "none",
+    fontFamily: "farro",
+    fontSize: "x-large",
+    margin: "0.5rem",
+  },
+})
 
 // Change name
 export default function RegisterSuccess() {
-
+  const classes = useStyles();
   let history = useHistory();
 
   const handleButton = () => {
@@ -15,11 +35,18 @@ export default function RegisterSuccess() {
   handleRedirect();
 
   return (
-    <div>
-      <h1>
-        success
-      </h1>
-      <button onClick={handleButton}>Done</button>
+    <div className={classes.background}>
+      <Container className={classes.page}>
+        <h1>Congratulations! Your account has been created.</h1>
+        <p>Click the button below to return to WorldPlay.</p>
+        <Button 
+          variant="contained"
+          color="primary"
+          onClick={handleButton}
+          className={classes.btn}>
+            <b>Go</b>
+        </Button>
+      </Container>
     </div>
   );
 }
