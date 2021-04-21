548097055a96f568f231a0f41823994d692787a8
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-04-21 14:40:19 +1000

    Settings page done

--- SourceCode_and_Documentation/frontend/src/pages/Settings.tsx
+++ SourceCode_and_Documentation/frontend/src/pages/Settings.tsx
@@ -1,28 +1,83 @@
 import React from "react";
 import { DashboardNav } from "../components/Navbar/DashboardNav";
-import { Container, makeStyles } from '@material-ui/core';
+import { Container, makeStyles, Switch, Select, MenuItem } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  page: {
+  backgroundDark: {
     backgroundColor: '#051c3f',
     height: '100%',
     color: 'white',
+  },
+  backgroundLight: {
+    backgroundColor: 'white',
+    height: '100%',
+    color: 'black',
+  },
+  page: {
+    padding: '7rem 0',
+  },
+  side: {
+    display: 'flex'
+  },
+  select: {
+    backgroundColor: 'white'
   }
 })
 
 const Settings = () => {
   const classes = useStyles();
-  console.log('account settings');
+  const [mode, setMode] = React.useState(false)
+  const [modeTitle, setModeTitle] = React.useState('Dark')
+  const [bg, setBg] = React.useState(classes.backgroundDark);
+  const [language, setLanguage] = React.useState('English');
 
-
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMode(!mode);
+    console.log(mode);
+    if (mode) {
+      setModeTitle('Dark');
+      setBg(classes.backgroundDark);
+    }
+    else {
+      setModeTitle('Light');
+      setBg(classes.backgroundLight);
+    };
+    
+  }
   return (
-    <Container maxWidth="lg" className={classes.page}>
+    <div className={bg}>
       <DashboardNav />
+      <Container className={classes.page}>
       <h1>Account Settings</h1>
       <div>
-
+          <h2>Light / Dark Mode</h2>
+          <div className={classes.side}>
+            <Switch
+                checked={mode}
+                onChange={handleChange}
+                name="mode"
+                color="primary"
+              />
+              <p>{modeTitle}</p>
+          </div>
+       
+        <div className={classes.side}>
+          <h2>Language:</h2>
+          <Select
+            labelId="Language"
+            id="languageSelect"
+            value={language}
+            className={classes.select}
+          >
+            <MenuItem value={10}>English</MenuItem>
+            <MenuItem value={20}>French</MenuItem>
+            <MenuItem value={30}>German</MenuItem>
+          </Select>
+        </div>
       </div>
-    </Container>
+      </Container>
+    </div>
+    
   );
 }
 
