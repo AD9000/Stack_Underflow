c3537005294aa4f19e96dcf0ce13a77655c20270
Jiaqi Zhu <blacker0lily311@gmail.com>
2021-04-26 09:47:07 +1000

    split basemodels from main.py

---
+++ SourceCode_and_Documentation/backend/schemas.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+from pydantic import BaseModel
+import json
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserRegister(UserBase):
+    password: str
+
+
+class User(UserBase):
+    logged_in: bool
+    # tags_owned: List[Tag] = []
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class TagInfo(BaseModel):
+    title: str
+    region: str
+    location: str
+    caption: str
+    song_uri: str  # url ?
+
+    # Pydantic vs UploadFile fix
+    # https://github.com/tiangolo/fastapi/issues/2257
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_to_json
+
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
